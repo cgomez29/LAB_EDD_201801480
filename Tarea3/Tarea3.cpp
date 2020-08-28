5f5f8b10c479565059100bfe0cd74273b17c75ad
Marvin <marvin1ronal@gmail.com>
2020-08-27 23:51:03 -0600

    Tarea3

--- Tarea3/Tarea3.cpp
+++ Tarea3/Tarea3.cpp
@@ -5,11 +5,15 @@ int main()
 {
     ArbolB* arbol = new ArbolB();
     arbol->insert(10);
-    arbol->insert(5);
-    arbol->insert(15);
     arbol->insert(1);
+    arbol->insert(20);
+    arbol->insert(15);
+    arbol->insert(8);
+    arbol->insert(96);
+    arbol->insert(23);
+    arbol->insert(4);
     arbol->insert(7);
-    arbol->insert(18);
+    arbol->insert(32);
 
     //arbol->delete_nodo(5);
     arbol->preOrden();
